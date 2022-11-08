ba111ae0bf6174768ca3abab07ed738aaa524480
emon-webdev <emon.webdev@gmail.com>
2022-11-08 20:58:27 +0600

    added dotenv

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ const Port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const serviceCollection = require("./data/services.json");
-
-// app.get("/services", (req, res) => {
-//   res.send(serviceCollection);
-// });
-
-//single service
-// app.get("/services/:id", (req, res) => {
-//   const id = req.params.id;
-//   const selectedService = serviceCollection.find((course) => course._id == id);
-//   res.send(selectedService);
-// });
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.m61comp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
